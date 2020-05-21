f27ff850e968a41c58428efa37961417ae7ff8bf
fahmi <fathurrohmanfahmi01@gmail.com>
2020-05-21 10:34:58 +0700

    coba

---
+++ app/src/main/java/com/example/trainning/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.trainning.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.trainning.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
